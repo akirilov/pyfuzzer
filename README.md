4bb6aba9f6cd995d748399bbc46c5a30cf86e303
akirilov <atanas_kirilov@hotmail.com>
2014-01-03 22:14:16 -0800

    Added ability to run fuzzed files.

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ A simple python fuzzer inspired by Charlie Miller's talk
 
 TODO:
 -----
-- Add ability to launch application with a given file
-- Save fuzzed files, deletion if no crash
-- Place above in loop
+- ~~Add ability to launch application with a given file~~
+- ~~Save fuzzed files, deletion if no crash~~
+- Place above in loop and detect crash
 - Add ability to use more than 1 template file (randomly select template)
 - Add ability to attach debugger on crash (WinDbg?)
 - Automate debugger with scipts to log useful info + minidump
--- fuzzer.py
+++ fuzzer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 
+import os, sys
 import math
 import random
+import shutil
+import time
 
 def FuzzString(string, factor):
     '''
@@ -9,7 +12,7 @@ def FuzzString(string, factor):
     substitutes them with new, random bytes, then returns the resulting string.
     '''
     buf = list(string)
-    numwrites = random.randrange(math.ceil((float(len(buf)) * factor / 100)))
+    numwrites = random.randrange(math.ceil((float(len(buf)) * factor)))
 
     for j in range(numwrites):
         rbyte = random.randrange(256)
@@ -19,8 +22,26 @@ def FuzzString(string, factor):
     return ''.join(buf)
 
 def main():
-    # test the fuzzer
-    print FuzzString("hello world", 50)
+    # Generate fuzzed file name
+    t_orig = sys.argv[1]
+    factor = float(sys.argv[2])
+    t_name = os.path.splitext(t_orig)[0]
+    t_ext = os.path.splitext(t_orig)[1]
+    t_fuzz = t_name + '_fuzzed' + t_ext
+
+    # Read original file into string, fuzz, and save
+    f_orig = open(t_orig, 'r')
+    str_orig = f_orig.read()
+    str_fuzz = FuzzString(str_orig, factor);
+    f_fuzz = open(t_fuzz, 'w')
+    f_fuzz.write(str_fuzz)
+    
+    # Close files
+    f_orig.close()
+    f_fuzz.close()
+    
+    # Start fuzzed file
+    os.system('open ' + t_fuzz)
 
 if __name__ == '__main__':
     main()
